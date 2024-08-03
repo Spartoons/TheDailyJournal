245c0a7a04da49f9523c505f3fdb3cdbf218f19f
Spartoons <aranoliveras@gmail.com>
2024-08-03 21:03:19 +0200

    Day 2: Paragraphs
    Today I have only work on the parapraphs function, it most very helpful the AI, because I was gonna make the hole implementation by hand given that because of lack of well research the other day, I though that the paragraph was not implemented like I wanted for the pdf on the ReportLab library. Thanks to ChatGPT and documentation I figure out that it was already implemented most of what I needed, so I just used that.

--- temp.py
+++ temp.py
@@ -11,6 +11,97 @@ from babel.dates import format_date
 from reportlab.pdfbase.pdfmetrics import stringWidth
 from reportlab.lib.styles import ParagraphStyle, getSampleStyleSheet
 from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
+from reportlab.lib.enums import TA_LEFT, TA_CENTER, TA_RIGHT, TA_JUSTIFY
+
+def draw_par(c, text, x, y, font="Helvetica", size=None, color=(0, 0, 0), alignment=1, weight=-1, height=-1, interline=1, max_rows=None):
+
+    # Determine alignment
+    if alignment == 0:
+        align = TA_LEFT
+    elif alignment == 2:
+        align = TA_RIGHT
+    elif alignment == 3:
+        align = TA_JUSTIFY
+    else:
+        align = TA_CENTER
+
+    # Set text color
+    c.setFillColorRGB(color[0], color[1], color[2])
+
+    # Adjust the font size to fit within the specified width
+    if size is None:
+        max_font_size = 200
+        min_font_size = 1
+        current_font_size = max_font_size
+        leading=current_font_size * interline
+
+        while current_font_size > min_font_size:
+            # Create a style for the paragraph
+            style = ParagraphStyle(
+                name='CustomStyle',
+                fontName=font,
+                fontSize=current_font_size,
+                leading=current_font_size * interline,
+                textColor=color,
+                alignment=align,
+                leftIndent=0,
+                rightIndent=0,
+                spaceBefore=0,
+                spaceAfter=0,
+                borderPadding=0,
+                allowOrphans=0,
+                allowWidows=0
+            )
+
+            # Create a Paragraph object
+            paragraph = Paragraph(text, style)
+
+            # Wrap the paragraph to fit within the specified width and height
+            if weight == -1:
+                weight = c._pagesize[0] - x  # Default to the right edge of the page
+            if height == -1:
+                height = c._pagesize[1] - y  # Default to the top edge of the page
+
+            width, para_height = paragraph.wrap(weight, height)
+            lines = para_height / (current_font_size * interline)
+
+            # Check if the paragraph fits within the specified width and height
+            if width <= weight and (max_rows is None or lines <= max_rows):
+                break
+
+            # Reduce font size if the text doesn't fit
+            current_font_size -= 0.5
+
+    else:
+        # Use the specified font size
+        current_font_size = size
+
+        style = ParagraphStyle(
+            name='CustomStyle',
+            fontName=font,
+            fontSize=current_font_size,
+            leading=current_font_size * interline,
+            textColor=color,
+            alignment=align,
+            leftIndent=0,
+            rightIndent=0,
+            spaceBefore=0,
+            spaceAfter=0,
+            borderPadding=0,
+            allowOrphans=0,
+            allowWidows=0
+        )
+
+        paragraph = Paragraph(text, style)
+        if weight == -1:
+            weight = c._pagesize[0] - x  # Default to the right edge of the page
+        if height == -1:
+            height = c._pagesize[1] - y  # Default to the top edge of the page
+
+        width, para_height = paragraph.wrap(weight, height)
+
+    # Draw the paragraph
+    paragraph.drawOn(c, x, y-para_height+(current_font_size*0.3))
 
 def dotted_line(x_start, x_end, y, c):
     # Draw a dotted line using small circles
@@ -31,6 +122,8 @@ width, height = A4
 
 # Add the title font
 pdfmetrics.registerFont(TTFont('Abibas', './assets/fonts/abibas/Abibas.ttf'))
+pdfmetrics.registerFont(TTFont('ArchivoBlack','./assets/fonts/ArchivoBlack-Regular.ttf'))
+pdfmetrics.registerFont(TTFont('Barlow','./assets/fonts/Barlow-Black.ttf'))
 
 c.setStrokeColorRGB(0,0,0)
 c.setLineWidth(2)
@@ -87,8 +180,8 @@ c.drawString(572.28 - width, 750.45, datn)
 c.setFont(template["title"]["font"], template["title"]["size"])
 c.drawString(template["title"]["x"], template["title"]["y"], template["title"]["text"])
 
-c.setStrokeColorRGB(0,0,0)
-c.setLineWidth(4)
+c.setStrokeColorRGB(70/256,31/256,124/256)
+c.setLineWidth(6.5)
 c.line(22.72,743,572.28,743)
 
 # Define styles
@@ -96,35 +189,19 @@ styles = getSampleStyleSheet()
 styleN = styles['Normal']
 
 lorem = "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas 'Letraset', las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum."
-para = Paragraph(lorem)
 
 # Draw Header
 c.setFont(template["header"]["font"], template["header"]["size"])
 c.drawString(template["header"]["x"], 600, "Header")
 
-# Draw Articles
-for i, article in enumerate(template["articles"]):
-    # Draw Article Title
-    c.setFont(article["title"]["font"], article["title"]["size"])
-    if i == 0:
-        c.drawString(article["title"]["x"], article["title"]["y"], "Article Title")
-    else:
-        c.drawString(article["title"]["x"], article["title"]["y"], article["title"]["text"])
+# Draw Image
+img_path = "assets/images/slkasjldaslñ.png"
+img_x = 234.87
+img_y = 101.7
+img_width = 337.4
+img_height = 338.8
+c.drawImage(img_path, img_x, img_y, img_width, img_height)
 
-    # Draw Article Body
-    c.setFont(article["body"]["font"], article["body"]["size"])
-    if i == 0:
-        c.drawString(article["body"]["x"], article["body"]["y"], "lorem Ipsum")
-    else:
-        c.drawString(article["body"]["x"], article["body"]["y"], article["body"]["text"])
-
-    # Draw Article Image if present
-    if "image" in article:
-        img_path = article["image"]["path"]
-        img_x = article["image"]["x"]
-        img_y = article["image"]["y"]
-        img_width = article["image"]["width"]
-        img_height = article["image"]["height"]
-        c.drawImage(img_path, img_x, img_y, img_width, img_height)
+draw_par(c,"SUPERMAN: THE MAN OF TOMORROW", 22.72, 733, alignment=1, font="Barlow", weight=549.56, max_rows=2)
 
 c.save()
